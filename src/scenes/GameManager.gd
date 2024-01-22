5f9b9f85771183ba71adc043493dfbceaa088a8a
Caleb Phillips <53085992+Lebyfochs@users.noreply.github.com>
2024-01-22 15:29:34 +0000

    Proto Movement
    Added prototypical movement, tiles and floor collision

---
+++ src/scenes/GameManager.gd
@@ -0,0 +1,18 @@
+extends Node
+
+
+var player_gas
+var king_gas
+var gas_depleter
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
+	player_gas = 0
+	king_gas = 10
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ src/scenes/player.gd
@@ -0,0 +1,72 @@
+extends CharacterBody2D
+
+@onready var _animation_player = $AnimatedSprite2D
+
+enum player_states
+{
+	IDLE,
+	RUNNING,
+	JUMPING,
+	ATTACKING,
+	COLLECTING
+}
+
+#var current_state = player_states.IDLE
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -500.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	
+	
+#	match player_states:
+#
+#		player_states.RUNNING:
+#
+#			_animation_player.play("run")
+#
+#		player_states.JUMPING:
+#			_animation_player.play("jump")
+#
+#		player_states.IDLE:
+#			_animation_player.play("idle")
+	
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		_animation_player.play("jump")
+		
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_just_released("jump"):
+		_animation_player.play("jump")
+		
+		
+		
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+		#current_state = player_states.RUNNING
+		_animation_player.play("run")
+		
+		
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		_animation_player.play("idle")
+		
+
+	move_and_slide()
+	#print(current_state)
+	
+			
+			
